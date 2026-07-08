555b305ab97f8493a866b924199a0513ede42aac
SabaSiva2916 <sabasiva2916@gmail.com>
2026-07-08 19:17:43 +0530

    Test wrk completed

--- tests/Sample.spec.js
+++ tests/Sample.spec.js
@@ -12,6 +12,13 @@ data.forEach((logindata) => {
     await page.locator("#password").fill(logindata.password);
     await page.locator("#login").click();
     await page.waitForTimeout(2000);
+    let s = "javascript";
+    let output = "";
+    for (let i = s.length() - 1; i >= 0; i--) {
+      output = output + s.charAt(i);
+    }
+    console.log(output);
+
     await browser.close();
   });
 });
